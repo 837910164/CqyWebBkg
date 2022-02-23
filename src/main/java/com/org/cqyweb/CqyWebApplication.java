df9017b698277b268ae5765b56dcd5459085591c
837910164 <2098055@163.com>
2022-02-23 10:37:50 +0800

    init

---
+++ src/main/java/com/org/cqyweb/CqyWebApplication.java
@@ -0,0 +1,13 @@
+package com.org.cqyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqyWebApplication.class, args);
+    }
+
+}
